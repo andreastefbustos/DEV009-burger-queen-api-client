a378a900e114907a740bcc1dd84dd91cfc9c84b8
Andrea Bustos <andreastefbustos@gmail.com>
2023-09-05 16:55:43 -0400

    Install react icons

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -6,15 +6,12 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  // Input,
   Button,
   DropdownTrigger,
   Dropdown,
   DropdownMenu,
   DropdownItem,
-  // Chip,
   User,
-  // Pagination,
 } from "@nextui-org/react";
 import { useLoaderData } from "react-router-dom";
 import { capitalize } from "./utils";
@@ -31,68 +28,70 @@ const statusOptions = [
   { name: "Admin", uid: "admin" },
 ];
 
-export function Dashboard() {
-  const users = Object.values(useLoaderData() as Record<string, User>) as User[];
-    const [filterValue, setFilterValue] = useState("");
-    const [statusFilter, setStatusFilter] = useState("all");
-
+function Dashboard() {
+    const users = Object.values(useLoaderData() as Record<string, User>) as User[];
+    const [statusFilter, setStatusFilter] = useState<Set<string | number>>(new Set());
+    
     const filteredItems = useMemo(() => {
         let filteredUsers = [...users];
 
-        if (statusFilter !== "all" && statusFilter.length !== statusOptions.length) {
-            console.log("filterValue", statusFilter)
-            filteredUsers = filteredUsers.filter((user) =>
-                Array.from(statusFilter).includes(user.role)
-            );
+        if (statusFilter.size !== 0 && statusFilter.size !== statusOptions.length) {
+            filteredUsers = filteredUsers.filter((user) => statusFilter.has(user.role));
         }
-
+        
         return filteredUsers;
-    }, [users, filterValue, statusFilter]);
-
-return (
-  // Center the div
-  <div>
-      <h1>Dashboard</h1>
+    }, [users, statusFilter]);
+    
+    return (
+    <div>
+      <h1>Users</h1>
+      <h1>Products</h1>
       <div className="flex gap-3">
-          <Dropdown>
-              <DropdownTrigger className="sm:flex">
-                  <Button endContent="" variant="flat">
-                  Role
-                  </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                  disallowEmptySelection
-                  aria-label="Table Columns"
-                  closeOnSelect={false}
-                  selectedKeys={statusFilter}
-                  selectionMode="multiple"
-                  onSelectionChange={setStatusFilter}>
-                      {statusOptions.map((status) => (
-                      <DropdownItem key={status.uid} className="capitalize">
-                          {capitalize(status.name)}
-                      </DropdownItem>
-                      ))}
-              </DropdownMenu>
-          </Dropdown>
-          <Button color="primary">
-          Add New
-          </Button>
+        <Dropdown>
+            <DropdownTrigger className="sm:flex">
+                <Button endContent="" variant="flat">Role</Button>
+            </DropdownTrigger>
+            <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={statusFilter}
+                selectionMode="multiple"
+                onSelectionChange={(selectedKeys) => {
+                    if (Array.isArray(selectedKeys)) {
+                        setStatusFilter(new Set(selectedKeys.filter(item => typeof item === 'string')));
+                    } else if (selectedKeys instanceof Set) {
+                        setStatusFilter(new Set(Array.from(selectedKeys).filter(item => typeof item === 'string')));
+                    }
+                }}
+            >
+                {statusOptions.map((status) => (
+                    <DropdownItem key={status.uid} className="capitalize">
+                        {capitalize(status.name)}
+                    </DropdownItem>
+                ))}
+            </DropdownMenu>
+        </Dropdown>
+        <Button color="primary">Add New</Button>
       </div>
       <Table aria-label="Example static collection table">
-          <TableHeader>
-              <TableColumn>Email</TableColumn>
-              <TableColumn>Role</TableColumn>
-          </TableHeader>
-          <TableBody>
-              {filteredItems.map((user) => (
-                  <TableRow key={user.email}>
-                      <TableCell>{user.email}</TableCell>
-                      <TableCell>{user.role}</TableCell>
-                  </TableRow>
-              ))}
-          </TableBody>
+        <TableHeader>
+            <TableColumn>EMAIL</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>ACTIONS</TableColumn>
+        </TableHeader>
+        <TableBody>
+            {filteredItems.map((user) => (
+                <TableRow key={user.email}>
+                    <TableCell>{user.email}</TableCell>
+                    <TableCell>{user.role}</TableCell>
+                    <TableCell>xxx</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
       </Table>
   </div>
 );
-
 }
+
+export { Dashboard };
