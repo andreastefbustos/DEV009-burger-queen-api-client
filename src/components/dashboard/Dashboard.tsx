54d8f053ba321ac868402b6d199665a3683d362d
Andrea Bustos <andreastefbustos@gmail.com>
2023-09-05 18:08:21 -0400

    add a new component for tje vertical dots icons and use them in the component dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,4 +1,6 @@
 import { useState, useMemo } from "react";
+// import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import { VerticalDotsIcon } from "./VerticalDotsIcons";
 import {
   Table,
   TableHeader,
@@ -48,7 +50,7 @@ function Dashboard() {
       <h1>Products</h1>
       <div className="flex gap-3">
         <Dropdown>
-            <DropdownTrigger className="sm:flex">
+            <DropdownTrigger className="sm:flex" aria-label="Role selection">
                 <Button endContent="" variant="flat">Role</Button>
             </DropdownTrigger>
             <DropdownMenu
@@ -85,7 +87,26 @@ function Dashboard() {
                 <TableRow key={user.email}>
                     <TableCell>{user.email}</TableCell>
                     <TableCell>{user.role}</TableCell>
-                    <TableCell>xxx</TableCell>
+                    <TableCell>
+                        {/* <FaEdit />
+                        <FaTrashAlt />   */}
+        <div className="relative flex justify-end items-center gap-2">
+        <Dropdown>
+              <DropdownTrigger>
+                <Button isIconOnly size="sm" variant="light" aria-label="More options">
+                  <VerticalDotsIcon className="text-default-300" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Opciones del usuario">
+                <DropdownItem>View</DropdownItem>
+                <DropdownItem>Edit</DropdownItem>
+                <DropdownItem>Delete</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+        </div>
+            
+          
+                    </TableCell>
                 </TableRow>
             ))}
         </TableBody>
