0902ab6948109406b08b5d50ed056952f04d0b88
Andrea Bustos <andreastefbustos@gmail.com>
2023-09-06 11:59:12 -0400

    add icons, tabs

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { useState, useMemo } from "react";
 // import { FaTrashAlt, FaEdit } from "react-icons/fa";
-import { VerticalDotsIcon } from "./VerticalDotsIcons";
+
 import {
   Table,
   TableHeader,
@@ -14,8 +14,15 @@ import {
   DropdownMenu,
   DropdownItem,
   User,
+  Tabs, 
+  Tab, 
+  Card, 
+  CardBody
 } from "@nextui-org/react";
 import { useLoaderData } from "react-router-dom";
+import { VerticalDotsIcon } from "./VerticalDotsIcons";
+import { ChevronDownIcon } from "./ChevronDownIcon";
+import { PlusIcon } from "./PlusIcon";
 import { capitalize } from "./utils";
 
 type User = {
@@ -30,7 +37,7 @@ const statusOptions = [
   { name: "Admin", uid: "admin" },
 ];
 
-function Dashboard() {
+function Dashboard(): JSX.Element {
     const users = Object.values(useLoaderData() as Record<string, User>) as User[];
     const [statusFilter, setStatusFilter] = useState<Set<string | number>>(new Set());
     
@@ -46,72 +53,83 @@ function Dashboard() {
     
     return (
     <div>
-      <h1>Users</h1>
-      <h1>Products</h1>
-      <div className="flex gap-3">
-        <Dropdown>
-            <DropdownTrigger className="sm:flex" aria-label="Role selection">
-                <Button endContent="" variant="flat">Role</Button>
-            </DropdownTrigger>
-            <DropdownMenu
-                disallowEmptySelection
-                aria-label="Table Columns"
-                closeOnSelect={false}
-                selectedKeys={statusFilter}
-                selectionMode="multiple"
-                onSelectionChange={(selectedKeys) => {
-                    if (Array.isArray(selectedKeys)) {
+      <div className="flex w-full flex-col">
+        <Tabs aria-label="Options">
+          <Tab key="users" title="Users">
+            <Card>
+              <CardBody>
+                <div className="flex gap-3">
+                  <Dropdown>
+                    <DropdownTrigger className="sm:flex" aria-label="Role selection">
+                      <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">Role</Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                    disallowEmptySelection
+                    aria-label="Table Columns"
+                    closeOnSelect={false}
+                    selectedKeys={statusFilter}
+                    selectionMode="multiple"
+                    onSelectionChange={(selectedKeys) => {
+                      if (Array.isArray(selectedKeys)) {
                         setStatusFilter(new Set(selectedKeys.filter(item => typeof item === 'string')));
-                    } else if (selectedKeys instanceof Set) {
+                      } else if (selectedKeys instanceof Set) {
                         setStatusFilter(new Set(Array.from(selectedKeys).filter(item => typeof item === 'string')));
-                    }
-                }}
-            >
-                {statusOptions.map((status) => (
-                    <DropdownItem key={status.uid} className="capitalize">
+                      }
+                    }}
+                    >
+                    {statusOptions.map((status) => (
+                      <DropdownItem key={status.uid} className="capitalize">
                         {capitalize(status.name)}
-                    </DropdownItem>
-                ))}
-            </DropdownMenu>
-        </Dropdown>
-        <Button color="primary">Add New</Button>
+                      </DropdownItem>
+                    ))}
+                    </DropdownMenu>
+                  </Dropdown>
+                  <Button color="primary" endContent={<PlusIcon />}>Add New</Button>
+                </div>
+                <Table aria-label="Example static collection table">
+                  <TableHeader>
+                    <TableColumn>EMAIL</TableColumn>
+                    <TableColumn>ROLE</TableColumn>
+                    <TableColumn>ACTIONS</TableColumn>
+                  </TableHeader>
+                  <TableBody>
+                    {filteredItems.map((user) => (
+                    <TableRow key={user.email}>
+                      <TableCell>{user.email}</TableCell>
+                      <TableCell>{user.role}</TableCell>
+                      <TableCell>
+                        <div className="relative flex justify-end items-center gap-2">
+                          <Dropdown>
+                            <DropdownTrigger>
+                              <Button isIconOnly size="sm" variant="light" aria-label="More options">
+                                <VerticalDotsIcon className="text-default-300" />
+                              </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu aria-label="Opciones del usuario">
+                              <DropdownItem>View</DropdownItem>
+                              <DropdownItem>Edit</DropdownItem>
+                              <DropdownItem>Delete</DropdownItem>
+                            </DropdownMenu>
+                          </Dropdown>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="products" title="Products">
+            <Card>
+              <CardBody>
+                
+              </CardBody>
+            </Card>
+          </Tab>
+        </Tabs>
       </div>
-      <Table aria-label="Example static collection table">
-        <TableHeader>
-            <TableColumn>EMAIL</TableColumn>
-            <TableColumn>ROLE</TableColumn>
-            <TableColumn>ACTIONS</TableColumn>
-        </TableHeader>
-        <TableBody>
-            {filteredItems.map((user) => (
-                <TableRow key={user.email}>
-                    <TableCell>{user.email}</TableCell>
-                    <TableCell>{user.role}</TableCell>
-                    <TableCell>
-                        {/* <FaEdit />
-                        <FaTrashAlt />   */}
-        <div className="relative flex justify-end items-center gap-2">
-        <Dropdown>
-              <DropdownTrigger>
-                <Button isIconOnly size="sm" variant="light" aria-label="More options">
-                  <VerticalDotsIcon className="text-default-300" />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu aria-label="Opciones del usuario">
-                <DropdownItem>View</DropdownItem>
-                <DropdownItem>Edit</DropdownItem>
-                <DropdownItem>Delete</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-        </div>
-            
-          
-                    </TableCell>
-                </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-  </div>
+    </div>
 );
 }
 
