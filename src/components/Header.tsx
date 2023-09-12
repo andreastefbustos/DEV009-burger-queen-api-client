733568932bc99e0a7946804b1d2d736d81d2f36e
Andrea Bustos <andreastefbustos@gmail.com>
2023-09-12 16:06:44 -0400

    CSS sign out

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
 import { FaUserCircle } from "react-icons/fa";
 import headerImg from "../assets/header.png";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
 
 function Header () {
     return (
         <div className="headerContainer" style={{backgroundImage: `url(${headerImg})`}}>
-            <FaUserCircle className="userIcon"/>
+            <Dropdown>
+                <DropdownTrigger>
+                    <div>
+                    <FaUserCircle className="userIcon"/>
+                    </div>
+                </DropdownTrigger>
+                <DropdownMenu aria-label="Static Actions">
+                    <DropdownItem key="delete" className="text-danger" color="danger">
+                        Sign out
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
         </div>
     );
 }
