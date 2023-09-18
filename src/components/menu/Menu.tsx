42b4796b49478f075020d7279d40bdf5134522c0
Andrea Bustos <andreastefbustos@gmail.com>
2023-09-18 11:55:18 -0400

    filter menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -10,6 +10,7 @@ from "@nextui-org/react";
 import { FaClipboardList } from "react-icons/fa";
 import { useLoaderData } from "react-router-dom";
 import './index.css'
+import { useMemo, useState } from "react";
 
 type Product = {
   id: number;
@@ -22,18 +23,33 @@ type Product = {
 
 export function Menu(): JSX.Element {
   const products = useLoaderData() as Product[];
+  const [activeTab, setActiveTab] = useState("desayunos"); // Establece un Tab inicial
+
+  const filteredProducts = useMemo(() => {
+    switch (activeTab) {
+      case "desayunos":
+        return products.filter(p => p.type === "desayuno");
+      case "almuerzosCenas":
+        return products.filter(p => p.type === "almuerzo_cena");
+      case "bebidas":
+        return products.filter(p => p.type === "bebida");
+      default:
+        return products;
+    }
+  }, [products, activeTab]);
+  
   return (
     <div>
       <div className="container-tabs">
-      <Tabs key="menu" size="lg" aria-label="Tabs menus">
-          <Tab key="desayunos" title="Desayunos"/>
-          <Tab key="almuerzosCenas" title="Almuerzos y Cenas"/>
-          <Tab key="bebidas" title="Bebidas"/>
+      <Tabs key="menu" size="lg" aria-label="Tabs menus" onSelectionChange={(key) => setActiveTab(key.toString())}>
+          <Tab key="desayunos" title="Desayunos" />
+          <Tab key="almuerzosCenas" title="Almuerzos y Cenas" />
+          <Tab key="bebidas" title="Bebidas" />
       </Tabs>
       </div>
       <div className="container-cards">
       <div className="cards">
-        {products.map((item, index) => (
+        {filteredProducts.map((item, index) => (
           <Card style={{ width: '200px', height: '320px' }} shadow="sm" key={index}>
             <CardBody className="overflow-visible p-0">
               <Image style={{height: '180px'}}
