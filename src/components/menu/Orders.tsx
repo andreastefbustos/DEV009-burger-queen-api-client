d53b67f8371dde9ca106d21bf04f9dcf85d68895
Andrea Bustos <andreastefbustos@gmail.com>
2023-09-21 23:24:58 -0400

    ADD EyeIcon to see order details when clicked and chage status color 'pending = warning'

--- src/components/menu/Orders.tsx
+++ src/components/menu/Orders.tsx
@@ -4,8 +4,12 @@ import {
     TableColumn, 
     TableBody, 
     TableRow, 
-    TableCell} from "@nextui-org/react";
+    TableCell,
+    Tooltip,
+    Chip} from "@nextui-org/react";
 import { useLoaderData } from "react-router-dom";
+import { EyeIcon } from "../../utilities/EyeIcon";
+
 
 type Product = {
     id: number;
@@ -36,15 +40,27 @@ function MyOrders() {
                     <TableColumn>TABLE</TableColumn>
                     <TableColumn>CLIENT</TableColumn>
                     <TableColumn>STATUS</TableColumn>
-                    <TableColumn>DETAILS</TableColumn>
+                    <TableColumn>ACTIONS</TableColumn>
                 </TableHeader>
                 <TableBody className="table-body">
                     {orders.map((order) => (
                         <TableRow key={order.userId}>
                             <TableCell>{order.table}</TableCell>
                             <TableCell>{order.client}</TableCell>
-                            <TableCell>{order.status}</TableCell>
-                            <TableCell>{order.status}</TableCell>
+                            <TableCell>
+                                <Chip className="capitalize" color="warning" size="sm" variant="flat">
+                                    {order.status}
+                                </Chip>
+                            </TableCell>
+                            <TableCell>
+                                <div className="relative flex items-center justify-center gap-2">
+                                    <Tooltip content="Details">
+                                        <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                            <EyeIcon />
+                                        </span>
+                                    </Tooltip>
+                                </div>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
